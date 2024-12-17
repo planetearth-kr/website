7d0cfca1c586a2f4c49ed0247702fa45fb0d8da1
Irochi <me@irochi.moe>
2024-12-17 10:02:27 +0900

    Resize padding

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ export default function PlanetEarthWebsite() {
           </div>
         </div>
       </section>
-      <section className="py-16">
+      <section className="pb-16">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {[...Array(6)].map((_, index) => (
