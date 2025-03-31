57d73146362362bd0e500d741e7c134284cf9c9e
Irochi <me@irochi.moe>
2025-03-31 10:55:22 +0900

    SEO optimize

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,12 @@ export const metadata: Metadata = {
   keywords: '마인크래프트, Minecraft, PlanetEarth, 타우니, 국전, 국가전쟁, 지구맛, 시즈워, 플어, 플래닛어스',
   icons: {
     icon: '/favicon.png'
-  }
+  },
+  openGraph: {
+    type: 'website',
+    title: 'PlanetEarth',
+    description: '오픈월드 타우니 지구서버 PlanetEarth',
+  },
 };
 
 export default function RootLayout({
