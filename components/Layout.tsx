b751766351c0fa1e4ed2fafc6948177c7ec2ae29
doyeon <cumber88@naver.com>
2026-05-07 17:06:39 +0900

    Revise company name

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -173,7 +173,7 @@ export function Footer() {
     <footer className="bg-white py-6 border-t border-gray-200">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-3 md:gap-0 text-gray-500 text-sm">
         <p>
-          2022-{new Date().getFullYear()} planetearth.kr. All rights reserved.
+          {new Date().getFullYear()} 플래닛네트워크 All rights reserved.
         </p>
         <div className="flex flex-col md:flex-row items-center gap-1 md:gap-4 text-center">
           <span>{t("disclaimer")}</span>
